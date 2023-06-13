44d2f610f9f535ee97500aa1f021ecf93e352f97
baekeys <rizqiakbarmem12@gmail.com>
2023-06-13 20:02:25 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,10 @@
+package universitas;
+
+public class Mahasiswa {
+    private String npm,nama,prodi;
+    
+    public getDetail(String npm,String nama, String prodi){
+        return;
+        
+    }
+}
---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universitas;
+
+/**
+ *
+ * @author rizqi
+ */
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        
+    }
+    
+}
