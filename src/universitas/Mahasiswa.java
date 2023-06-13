d0891f1f4af43d85131b59f80aea853f8dabf84b
baekeys <rizqiakbarmem12@gmail.com>
2023-06-13 21:31:53 +0800

    Membuat class mahasiswa dan mahasiswa beraksi

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -3,8 +3,35 @@ package universitas;
 public class Mahasiswa {
     private String npm,nama,prodi;
     
-    public getDetail(String npm,String nama, String prodi){
-        return;
+    public Mahasiswa(String npm,String nama,String prodi){
+       this.npm = npm;
+       this.nama = nama;
+       this.prodi=prodi;
+   }
+    public String getDetail(){
+        System.out.println("npm"+npm);
+        System.out.println("nama"+nama);
+        System.out.println("prodi"+prodi);
+        return null;
         
+    }
+    
+    void setNPM(String ubahnpm){
+        npm=ubahnpm;
+    }
+   void setNAMA(String ubahnama){
+        nama=ubahnama;
+    }
+   void setPRODI(String ubahprodi){
+        prodi=ubahprodi;
+    }
+   String getNPM(){
+        return npm;
+    }
+   String getNAMA(){
+        return nama;
+    }
+   String getPRODI(){
+        return prodi;
     }
 }
